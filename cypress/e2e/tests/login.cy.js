44520a041a8cf2973a3c0d8704f10af3cb887765
dmeegodai <thanudilan@ymail.com>
2024-09-19 09:05:07 +0530

    Page Login code committed

---
+++ cypress/e2e/tests/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+describe("Orange HRM POM Testing", () => {
+  it("Page Login", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.xpath("//input[@name='username']").should("be.visible");
+
+    cy.xpath("//input[@name='username']").type("Admin");
+    cy.xpath("//input[@name='password']").type("admin123");
+    cy.get(".oxd-button").click();
+    cy.contains("Dashboard").should("be.visible");
+  });
+});
